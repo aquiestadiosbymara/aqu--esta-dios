3f9430340714bf96b5f764c2e7e8a67f757a91ed
aquiestadiosbymara <aquiestadiosbymara@hotmail.com>
2026-08-04 17:43:17 +0200

    Add custom cursor movement functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const cursorLight = document.querySelector(".cursor-light");
 
+document.addEventListener("mousemove", (e) => {
+
+    cursorLight.style.left = e.clientX + "px";
+    cursorLight.style.top = e.clientY + "px";
+
+});
